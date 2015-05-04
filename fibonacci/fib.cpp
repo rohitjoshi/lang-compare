eac9e74609f0a72258cda0767a19507de558487b
Rohit Joshi <rohit.c.joshi@gmail.com>
2015-05-04 13:37:00 -0400

    pony support

--- fibonacci/fib.cpp
+++ fibonacci/fib.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cstdlib>
 
  long fib(long n) 
 {
--- fibonacci/fib.rs
+++ fibonacci/fib.rs
@@ -1,5 +1,4 @@
-use std::os;
-
+use std::*;
 // fib returns a function that returns
 // successive Fibonacci numbers.
 fn fib(n: u64) -> u64 {
@@ -10,10 +9,11 @@ fn fib(n: u64) -> u64 {
 }
 
 fn main() {
-    let args = os::args();
-    let args = args.as_slice();
-	let n :u64 = from_str::<u64>(args[1].as_slice()).unwrap();
-
+    let args: Vec<_> = env::args().collect();
+    //let args = os::args();
+    //let args = args.as_slice();
+	//let n :u64 = from_str::<u64>(args[1].as_slice()).unwrap();
+    let n: u64 = args[1].parse::<u64>().unwrap();
     let result = fib(n);
 	println!("LANGUAGE Rust: {0}", result);
 }
